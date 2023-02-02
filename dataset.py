571c180d3b3a5425bf1186732ca841f048f4b94b
JC Liu <ljcc0930@qq.com>
2023-02-02 17:37:56 -0500

    update encode decode

--- dataset.py
+++ dataset.py
@@ -1,14 +1,15 @@
 import os
-import itertools
+import numpy as np
 
 import utils
 
 
 class Polarity:
-    def __init__(self, num_folds, data_dir):
+    def __init__(self, n_folds, data_dir):
         self.dataset_url = "https://www.cs.cornell.edu/people/pabo/movie-review-data/review_polarity.tar.gz"
         self.data_dir = data_dir
-        self.num_folds = num_folds
+        self.n_folds = n_folds
+        self.n_class = 2
 
         self.load_data()
 
@@ -24,45 +25,82 @@ class Polarity:
 
         dirs = [os.path.join(dataset_dir, cls) for cls in ['pos', 'neg']]
 
-        self.folds = [[] for x in range(self.num_folds)], [
-            [] for x in range(self.num_folds)]
+        self.corpus = []
+
+        self.folds = [[] for x in range(self.n_folds)], [
+            [] for x in range(self.n_folds)]
 
         for label, class_dir in enumerate(dirs):
             data = []
             for file in os.scandir(class_dir):
                 if file.is_file():
                     if file.name.endswith(".txt"):
-                        data.append(utils.load_from_txt(file.path))
+                        context = utils.load_from_txt(file.path)
+                        data.append(context)
+                        self.corpus += utils.concat_lists(*context)
 
             cur = 0
-            num_data = len(data)
+            n_data = len(data)
             for idx, (fold0, fold1) in enumerate(zip(*self.folds)):
-                nex = cur + (num_data - cur) // (self.num_folds - idx)
+                nex = cur + (n_data - cur) // (self.n_folds - idx)
                 fold0.extend(data[cur: nex])
                 fold1.extend([label] * (nex - cur))
                 cur = nex
 
+        self.corpus = list(np.unique(self.corpus))
+        self.n_corpus = len(self.corpus)
+        self.encode_mapping = {s: i for i, s in enumerate(self.corpus)}
+        self._is_encoded = False
+
     def get_datasets(self, fold):
-        train_text = list(itertools.chain(
-            *self.folds[0][:fold], *self.folds[0][fold + 1:]))
-        train_label = list(itertools.chain(
-            *self.folds[1][:fold], *self.folds[1][fold + 1:]))
+        train_text = utils.concat_lists(
+            *self.folds[0][:fold], *self.folds[0][fold + 1:])
+        train_label = utils.concat_lists(
+            *self.folds[1][:fold], *self.folds[1][fold + 1:])
         test_text = self.folds[0][fold]
         test_label = self.folds[1][fold]
         return train_text, train_label, test_text, test_label
 
+    def encode(self):
+        if self._is_encoded:
+            return
 
-def get_dataset(dataset, num_folds=3, data_dir='data'):
-    if dataset == "polarity":
-        return Polarity(num_folds, data_dir)
+        for fold in self.folds[0]:
+            for doc in fold:
+                for line in doc:
+                    for idx, word in enumerate(line):
+                        line[idx] = self.encode_mapping[word]
+
+        self._is_encoded = True
+
+    def decode(self):
+        if not self._is_encoded:
+            return
+
+        for fold in self.folds[0]:
+            for doc in fold:
+                for line in doc:
+                    for idx, item in enumerate(line):
+                        line[idx] = self.corpus[item]
+
+        self._is_encoded = False
+
+
+def get_dataset(name, n_folds=3, data_dir='data'):
+    if name == "polarity":
+        return Polarity(n_folds, data_dir)
+    else:
+        raise NotImplementedError("Dataset '{}' not implemented!".format(name))
 
 
 if __name__ == "__main__":
     n = 3
-    ds = get_dataset("polarity", num_folds=n)
+    ds = get_dataset("polarity", n_folds=n)
     for fold in range(3):
         train_text, train_label, test_text, test_label = ds.get_datasets(fold)
         print(len(train_text))
         print(len(train_label))
         print(len(test_text))
         print(len(test_label))
+    ds.encode()
+    ds.decode()
