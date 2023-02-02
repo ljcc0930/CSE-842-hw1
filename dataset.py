fe137a4fc30304a27af9b64b42ad5d3477bb753f
JC Liu <ljcc0930@qq.com>
2023-02-02 14:49:11 -0500

    upd dataset loading

--- dataset.py
+++ dataset.py
@@ -1,30 +1,68 @@
 import os
+import itertools
 
-import torch
 import utils
+
 
 class Polarity:
     def __init__(self, num_folds, data_dir):
-        dataset_dir = os.path.join(data_dir, 'txt_sentoken')
-        self.get_data(dataset_dir)
+        self.dataset_url = "https://www.cs.cornell.edu/people/pabo/movie-review-data/review_polarity.tar.gz"
+        self.data_dir = data_dir
         self.num_folds = num_folds
 
-    def get_data(self, dataset_dir):
-        dirs = [os.path.join(dataset_dir, cls) for cls in ['pro', 'neg']]
+        self.load_data()
 
-        data = [[], []]
+    def load_data(self):
+        dataset_url = self.dataset_url
+        data_dir = self.data_dir
+        dataset_compress_name = dataset_url.split('/')[-1]
+
+        utils.ensure_download_data(
+            dataset_url, dataset_compress_name, data_dir)
+
+        dataset_dir = os.path.join(data_dir, 'txt_sentoken')
+
+        dirs = [os.path.join(dataset_dir, cls) for cls in ['pos', 'neg']]
+
+        self.folds = [[] for x in range(self.num_folds)], [
+            [] for x in range(self.num_folds)]
+
         for label, class_dir in enumerate(dirs):
-            for name in os.scandir(class_dir):
-                if name.endswith('.txt'):
-                    path = os.path.join(class_dir, name)
-                    data[label] += utils.load_txt_file(path)
-        self.folds = []
-        
+            data = []
+            for file in os.scandir(class_dir):
+                if file.is_file():
+                    if file.name.endswith(".txt"):
+                        data.append(utils.load_from_txt(file.path))
+
+            cur = 0
+            num_data = len(data)
+            for idx, (fold0, fold1) in enumerate(zip(*self.folds)):
+                nex = cur + (num_data - cur) // (self.num_folds - idx)
+                fold0.extend(data[cur: nex])
+                fold1.extend([label] * (nex - cur))
+                cur = nex
+
+    def get_datasets(self, fold):
+        train_text = list(itertools.chain(
+            *self.folds[0][:fold], *self.folds[0][fold + 1:]))
+        train_label = list(itertools.chain(
+            *self.folds[1][:fold], *self.folds[1][fold + 1:]))
+        test_text = self.folds[0][fold]
+        test_label = self.folds[1][fold]
+        return train_text, train_label, test_text, test_label
 
 
-def get_dataset(dataset, num_folds = 3, data_dir = 'data'):
+def get_dataset(dataset, num_folds=3, data_dir='data'):
     if dataset == "polarity":
         return Polarity(num_folds, data_dir)
 
+
 if __name__ == "__main__":
-    get_dataset("polarity")
+    n = 3
+    ds = get_dataset("polarity", num_folds=n)
+    for fold in range(3):
+        train_text, train_label, test_text, test_label = ds.get_datasets(fold)
+        print(len(train_text))
+        print(len(train_label))
+        print(len(test_text))
+        print(len(test_label))
--- utils.py
+++ utils.py
@@ -1,2 +1,19 @@
+import os
+import wget
+import tarfile
+
+
+def ensure_download_data(url, name, dir):
+    file_path = os.path.join(dir, name)
+    if not os.path.exists(file_path):
+        os.makedirs(dir, exist_ok=True)
+        wget.download(url=url, out=dir)
+
+        file = tarfile.open(file_path)
+        file.extractall(dir)
+
+
 def load_from_txt(path):
-    return 
+    fin = open(path, 'r')
+    lines = fin.readlines()
+    return lines
