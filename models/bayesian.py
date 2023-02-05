e4f57d06c2fab4f37d88338ac0887969051aa45b
JC Liu <ljcc0930@qq.com>
2023-02-05 15:39:23 -0500

    Fix Bayesian

--- models/bayesian.py
+++ models/bayesian.py
@@ -15,19 +15,17 @@ class NaiveBayes:
     def update(self, docs, labels):
         for doc, label in zip(docs, labels):
             self.N_c[label] += 1
-            # for line in doc:
-            #     for word in line:
-            #         self.N_w_c[word][label] += 1
-            self.N_w_c[utils.concat_lists(*doc), label] += 1
+            for line in doc:
+                for word in line:
+                    self.N_w_c[word][label] += 1
 
     def predict(self, docs, k=0):
         P_c = self.N_c / self.N_c.sum()
         P_w_c = (self.N_w_c + k) / (self.N_w_c + k).sum(axis=0)
+        lP_c = np.log(P_c)
         lP_w_c = np.log(P_w_c)
 
-        prob = np.log(P_c)
-
-        prob = [np.sum(lP_w_c[utils.concat_lists(*doc)], axis=0)
+        prob = [np.sum(lP_w_c[utils.concat_lists(*doc)], axis=0) + lP_c
                 for doc in docs]
 
         return np.argmax(prob, axis=1)
